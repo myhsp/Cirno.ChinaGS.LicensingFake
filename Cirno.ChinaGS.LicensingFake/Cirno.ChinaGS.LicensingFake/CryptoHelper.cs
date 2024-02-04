f5428a4c507e04347b0d2912211e22795faf6997
konata233 <52315359+konata233@users.noreply.github.com>
2024-02-04 16:10:42 +0800

    Add files via upload

---
+++ Cirno.ChinaGS.LicensingFake/Cirno.ChinaGS.LicensingFake/CryptoHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirno.ChinaGS.LicensingFake
+{
+    public class CryptoHelper
+    {
+        public string xmlStr;
+
+        public CryptoHelper()
+        {
+            this.xmlStr = this.ReadLic();
+        }
+
+        public CryptoHelper(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                this.xmlStr = new StreamReader(filename, Encoding.Default).ReadToEnd();
+            }
+            else
+            {
+                this.xmlStr = this.ReadLic();
+            }
+        }
+        
+        public string ReadLic()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream manifestResourceStream = assembly.GetManifestResourceStream("Cirno.ChinaGS.LicensingFake.Resources.License.lic");
+            StreamReader streamReader = new StreamReader(manifestResourceStream);
+
+            return streamReader.ReadToEnd();
+        }
+
+        public string Encrypt(string addonSymbolicName)
+        {
+            using (RSACryptoServiceProvider provider = new RSACryptoServiceProvider())
+            {
+                provider.FromXmlString(this.xmlStr);
+                RSAPKCS1SignatureFormatter formatter = new RSAPKCS1SignatureFormatter(provider);
+                formatter.SetHashAlgorithm("SHA1");
+
+                SHA1Managed sha1 = new SHA1Managed();
+                byte[] rgbHash = sha1.ComputeHash(Encoding.ASCII.GetBytes(addonSymbolicName));
+
+                byte[] signature = formatter.CreateSignature(rgbHash);
+                string b64str = Convert.ToBase64String(signature);
+
+                return b64str;
+            }
+        }
+
+        public bool Verify(string addonSymbolicName, string crypted)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider provider = new RSACryptoServiceProvider())
+                {
+                    provider.FromXmlString(this.xmlStr);
+                    RSAPKCS1SignatureDeformatter deformatter = new RSAPKCS1SignatureDeformatter(provider);
+                    deformatter.SetHashAlgorithm("SHA1");
+
+                    SHA1Managed sha1 = new SHA1Managed();
+                    byte[] rgbHash = sha1.ComputeHash(Encoding.ASCII.GetBytes(addonSymbolicName));
+                    return deformatter.VerifySignature(rgbHash, Convert.FromBase64String(crypted));
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Cirno.ChinaGS.LicensingFake/Cirno.ChinaGS.LicensingFake/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cirno.ChinaGS.LicensingFake
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            CryptoHelper helper = new CryptoHelper(License.Text);
+            this.TextOutput.Text = helper.Encrypt(this.TextInput.Text);
+        }
+
+        private void LicensingSelect_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "License files (.lic)|*.lic|All files (*.*)|*.*"
+            };
+
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                License.Text = dialog.FileName;
+            }
+        }
+
+        private void Verify_Click(object sender, RoutedEventArgs e)
+        {
+            CryptoHelper helper = new CryptoHelper(License.Text);
+            bool result = helper.Verify(this.TextInput.Text, this.TextOutput.Text);
+            if (result)
+            {
+                MessageBox.Show("Success!\n\n=>AddonSymbolicName: " + this.TextInput.Text +
+                    "\n\n=> SignedAddonSymbolicName: " + this.TextOutput.Text, "Verify");
+            }
+            else
+            {
+                MessageBox.Show("Failed!\n\n=> AddonSymbolicName: " + this.TextInput.Text +
+                    "\n\n=> SignedAddonSymbolicName: " + this.TextOutput.Text, "Verify");
+            }
+        }
+    }
+}
